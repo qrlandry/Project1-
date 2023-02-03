2a59569e711a831faf602f3136980c64239b54ae
qrlandry <qrlandry@gmail.com>
2023-02-03 15:29:25 -0500

    basic java for holes in tubes and more css styling

--- script.js
+++ script.js
@@ -1,10 +1,17 @@
 // bring in elements from HTML
 const tube = document.querySelector('#tube');
 const hole = document.querySelector('#hole');
-
+const score = document.querySelector('#score');
+const player = document.querySelector('#player');
+const cscore = 0;
+score.innerHTML = cscore;
+const jumps = 0;
 // every time the animation runs this function that will cut
 // holes in the tubes will run
 hole.addEventListener('animationiteration', () => {
-    let rdm = ((Math.random()* 400)+100)
-    tube.style.top = rdm + "px";
+    let rdm = -((Math.random()* 400)+100)
+    hole.style.top = rdm + "px";
+    cscore++;
 })
+
+
